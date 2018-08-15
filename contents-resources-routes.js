c5dd34764f06bd694bec1d0d7810d07568754c0a
Willy Nugraha <willymiiee@gmail.com>
2018-08-16 01:02:13 +0800

    reformat & add logic on get single user

--- contents-resources-routes.js
+++ contents-resources-routes.js
@@ -1,81 +1,80 @@
-const path = require('path');
-const dotenv = require('dotenv');
-const express = require('express');
-const firebaseAdmin = require('./commons/firebaseAdmin');
-const algoliasearch = require('algoliasearch');
-const Geode = require('geode');
+const path = require('path')
+const dotenv = require('dotenv')
+const express = require('express')
+const firebaseAdmin = require('./commons/firebaseAdmin')
+const algoliasearch = require('algoliasearch')
+const Geode = require('geode')
 
-dotenv.config({ path: path.dirname(require.main.filename) + '/.env' });
+dotenv.config({ path: path.dirname(require.main.filename) + '/.env' })
 
-const router = express.Router();
-const algolia = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY);
-const indexPhotographers = algolia.initIndex(process.env.ALGOLIA_INDEX_PHOTOGRAPHERS);
-const indexUsers = algolia.initIndex(process.env.ALGOLIA_INDEX_USERS);
+const router = express.Router()
+const algolia = algoliasearch(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY)
+const indexPhotographers = algolia.initIndex(process.env.ALGOLIA_INDEX_PHOTOGRAPHERS)
+const indexUsers = algolia.initIndex(process.env.ALGOLIA_INDEX_USERS)
 
-function fetchCurrencies() {
+function fetchCurrencies () {
   return new Promise(function (resolve, reject) {
     firebaseAdmin
       .database()
       .ref('currency_exchange_rates')
       .once('value')
       .then(function (snapshot) {
-        resolve(snapshot.val());
+        resolve(snapshot.val())
       })
       .catch(function (error) {
-        reject(error);
-      });
-  });
+        reject(error)
+      })
+  })
 }
 
-function convertPriceCurrency(rows, priceKey, allLocalRates, currency) {
+function convertPriceCurrency (rows, priceKey, allLocalRates, currency) {
   if (typeof rows !== 'undefined' && rows && rows.length > 0) {
     return rows.map(function (item) {
-      const useCurrency = !currency ? item.currency : currency;
-      const IDR_USD_Rates = allLocalRates['IDRUSD'];
-      const IDRRates = allLocalRates['IDR' + useCurrency];
-      const USDRates = allLocalRates['USD' + useCurrency];
+      const useCurrency = !currency ? item.currency : currency
+      const rates = allLocalRates['IDRUSD']
+      const IDRRates = allLocalRates['IDR' + useCurrency]
+      const USDRates = allLocalRates['USD' + useCurrency]
 
       // Get IDR rates of current local rates first.
-      const inIDR = Math.round(item[priceKey] / IDRRates);
-      item[priceKey + 'IDR'] = inIDR;
+      const inIDR = Math.round(item[priceKey] / IDRRates)
+      item[priceKey + 'IDR'] = inIDR
 
       // Use IDR rates as a base value to convert to USD
-      item[priceKey + 'USD'] = Math.round(IDR_USD_Rates * inIDR);
+      item[priceKey + 'USD'] = Math.round(rates * inIDR)
 
       // Use original price (not converted to IDR before) to convert to USD.
       // This is just an additional optional information.
-      item[priceKey + 'USD2'] = Math.round(item[priceKey] / USDRates);
+      item[priceKey + 'USD2'] = Math.round(item[priceKey] / USDRates)
 
-      return item;
-    });
+      return item
+    })
   }
 
-  return [];
+  return []
 }
 
 router.get('/photographers', function (request, response) {
-  var destination = request.query['filter']['destination'];
-  var date = request.query['filter']['date'];
-  var search = {
+  const destination = request.query['filter']['destination']
+  const date = request.query['filter']['date']
+  const search = {
     query: destination,
     hitsPerPage: process.env.ALGOLIA_HITS_PER_PAGE,
     page: request.query['filter']['page'],
     attributesToHighlight: ['locationMerge']
-  };
+  }
 
   if (date !== '') {
-    search.filters = 'NOT notAvailableDates:' + date;
+    search.filters = 'NOT notAvailableDates:' + date
   }
 
   fetchCurrencies()
     .then(function (currencies) {
-      indexPhotographers.search(search, function searchDone(error, content) {
+      indexPhotographers.search(search, function searchDone (error, content) {
         if (error) {
-          console.error(error);
-          response.json({ data: [] });
-
+          console.error(error)
+          response.json({ data: [] })
         } else {
-          const convertedData = convertPriceCurrency(content.hits, 'priceStartFrom', currencies);
+          const convertedData = convertPriceCurrency(content.hits, 'priceStartFrom', currencies)
           response.json({
             data: convertedData,
             metaInfo: {
@@ -84,20 +83,21 @@ router.get('/photographers', function (request, response) {
               nbPages: content.nbPages,
               hitsPerPage: content.hitsPerPage
             }
-          });
+          })
         }
-      });
+      })
     })
     .catch(function (error) {
-      console.error(error);
-      response.json({ data: [] });
-    });
-});
+      console.error(error)
+      response.json({ data: [] })
+    })
+})
 
 router.get('/admin/users', function (request, response) {
   const filterQueryObject = request.query['filter']
   const userType = request.query['userType'] === 'p' ? 'photographer' : 'traveller'
-  let filters = query = ''
+  let query = ''
+  let filters = ''
 
   if (typeof filterQueryObject !== 'undefined') {
     Object.keys(filterQueryObject).forEach(function (key) {
@@ -110,7 +110,7 @@ router.get('/admin/users', function (request, response) {
     })
   }
 
-  var search = {
+  const search = {
     query: query,
     filters: 'userType:' + userType + ' ' + filters,
     hitsPerPage: request.query['limit'] ? request.query['limit'] : 50,
@@ -120,13 +120,12 @@ router.get('/admin/users', function (request, response) {
 
   fetchCurrencies()
     .then(function (currencies) {
-      indexUsers.search(search, function searchDone(error, content) {
+      indexUsers.search(search, function searchDone (error, content) {
         if (error) {
-          console.error(error);
-          response.json({ data: [] });
-
+          console.error(error)
+          response.json({ data: [] })
         } else {
-          const convertedData = convertPriceCurrency(content.hits, 'priceStartFrom', currencies);
+          const convertedData = convertPriceCurrency(content.hits, 'priceStartFrom', currencies)
           response.json({
             data: convertedData,
             metaInfo: {
@@ -135,130 +134,70 @@ router.get('/admin/users', function (request, response) {
               nbPages: content.nbPages,
               hitsPerPage: content.hitsPerPage
             }
-          });
+          })
         }
-      });
+      })
     })
     .catch(function (error) {
-      console.error(error);
-      response.json({ data: [] });
-    });
-});
+      console.error(error)
+      response.json({ data: [] })
+    })
+})
 
 router.get('/topPhotographers', function (request, response) {
-  indexPhotographers.search({
-    attributesToHighlight: ['locationMerge'],
-    facets: ['topPhotographer', 'userType'],
-    facetFilters: [['topPhotographer:true']]
-  }, function searchDone(error, content) {
-    if (error) {
-      console.error(error);
-      response.json({ data: [] });
-    } else {
-      response.json({ data: content.hits });
+  indexPhotographers.search(
+    {
+      attributesToHighlight: ['locationMerge'],
+      facets: ['topPhotographer', 'userType'],
+      facetFilters: [['topPhotographer:true']]
+    },
+    function searchDone (error, content) {
+      if (error) {
+        console.error(error)
+        response.json({ data: [] })
+      } else {
+        response.json({ data: content.hits })
+      }
     }
-  });
-});
+  )
+})
 
 router.get('/photographers/:uid', function (request, response) {
-  fetchCurrencies()
-    .then(function (currencies) {
-      const uid = request.params.uid;
-      const db = firebaseAdmin.database();
-
-      db
-        .ref('photographer_service_information')
-        .child(uid)
-        .once('value')
-        .then(function (data) {
-          const photographerServiceInformationData = data.val();
-          if (photographerServiceInformationData) {
-            db
-              .ref('user_metadata')
-              .child(uid)
-              .once('value')
-              .then(function (userMetadataData) {
-                const userMetadataDataVal = userMetadataData.val();
-                const userMetadataDataItem = [userMetadataDataVal];
-                const packagesPriceModified = convertPriceCurrency(
-                  photographerServiceInformationData.packagesPrice,
-                  'price',
-                  currencies,
-                  userMetadataDataVal.currency
-                );
-
-                photographerServiceInformationData.packagesPrice = packagesPriceModified;
-                photographerServiceInformationData.userMetadata = convertPriceCurrency(userMetadataDataItem, 'priceStartFrom', currencies)[0];
-                response.json({ data: photographerServiceInformationData });
-              });
-
-          } else {
-            response.json({ data: {} });
-          }
-        });
-    })
-    .catch(function (error) {
-      console.error(error);
-      response.json({ data: {} });
-    });
-});
-
-router.get('/cities', function (request, response) {
-  const qry = request.query['kwd'];
-  const countryCode = request.query['countryCode'];
-  const continent = request.query['continent'];
-  const geo = new Geode('okaprinarjaya', { countryCode: countryCode });
-
-  geo.search({ q: qry, continentCode: continent, featureClass: 'A' }, function (error, result) {
-    if (error) {
-      console.log(error);
-    } else {
-      var results = [];
-      result.geonames.forEach(function (item) {
-        results.push(
-          {
-            value: item.toponymName,
-            label: item.toponymName,
-            adm1: item.adminName1
-          }
-        );
-      });
-      response.json({ data: results });
-    }
-  });
-});
-
-router.get('/locations', function (request, response) {
-  indexPhotographers.search({
-    query: request.query.keyword,
-    distinct: true,
-    attributesToHighlight: ['countryName'],
-    attributesToRetrieve: ['countryName', 'locationAdmLevel1', 'locationAdmLevel2']
-  }, function searchDone(error, content) {
-    if (error) {
-      console.error(error);
-      response.json({ data: [] });
-    } else {
-      const results = content.hits.map(function (item) {
-        return { label: item.locationAdmLevel2 + ', ' + item.locationAdmLevel1 + ', ' + item.countryName };
-      });
-      response.json({ data: results });
-    }
-  });
-});
-
-router.get('/users/:uid', function (request, response) {
   fetchCurrencies()
     .then(function (currencies) {
       const uid = request.params.uid
       const db = firebaseAdmin.database()
 
-      db
-        .ref('user_metadata')
+      db.ref('photographer_service_information')
         .child(uid)
         .once('value')
         .then(function (data) {
-          response.json({ data: data.val() })
+          const photographerServiceInformationData = data.val()
+          if (photographerServiceInformationData) {
+            db.ref('user_metadata')
+              .child(uid)
+              .once('value')
+              .then(function (userMetadataData) {
+                const userMetadataDataVal = userMetadataData.val()
+                const userMetadataDataItem = [userMetadataDataVal]
+                const packagesPriceModified = convertPriceCurrency(
+                  photographerServiceInformationData.packagesPrice,
+                  'price',
+                  currencies,
+                  userMetadataDataVal.currency
+                )
+
+                photographerServiceInformationData.packagesPrice = packagesPriceModified
+                photographerServiceInformationData.userMetadata = convertPriceCurrency(
+                  userMetadataDataItem,
+                  'priceStartFrom',
+                  currencies
+                )[0]
+                response.json({ data: photographerServiceInformationData })
+              })
+          } else {
+            response.json({ data: {} })
+          }
         })
     })
     .catch(function (error) {
@@ -267,4 +206,77 @@ router.get('/users/:uid', function (request, response) {
     })
 })
 
-module.exports = router;
+router.get('/cities', function (request, response) {
+  const qry = request.query['kwd']
+  const countryCode = request.query['countryCode']
+  const continent = request.query['continent']
+  const geo = new Geode('okaprinarjaya', { countryCode: countryCode })
+
+  geo.search({ q: qry, continentCode: continent, featureClass: 'A' }, function (error, result) {
+    if (error) {
+      console.log(error)
+    } else {
+      let results = []
+      result.geonames.forEach(function (item) {
+        results.push({
+          value: item.toponymName,
+          label: item.toponymName,
+          adm1: item.adminName1
+        })
+      })
+      response.json({ data: results })
+    }
+  })
+})
+
+router.get('/locations', function (request, response) {
+  indexPhotographers.search(
+    {
+      query: request.query.keyword,
+      distinct: true,
+      attributesToHighlight: ['countryName'],
+      attributesToRetrieve: ['countryName', 'locationAdmLevel1', 'locationAdmLevel2']
+    },
+    function searchDone (error, content) {
+      if (error) {
+        console.error(error)
+        response.json({ data: [] })
+      } else {
+        const results = content.hits.map(function (item) {
+          return { label: item.locationAdmLevel2 + ', ' + item.locationAdmLevel1 + ', ' + item.countryName }
+        })
+        response.json({ data: results })
+      }
+    }
+  )
+})
+
+router.get('/users/:uid', function (request, response) {
+  fetchCurrencies()
+    .then(function (currencies) {
+      const uid = request.params.uid
+      const db = firebaseAdmin.database()
+
+      db.ref('user_metadata')
+        .child(uid)
+        .once('value')
+        .then(function (data) {
+          let userDetail = data.val()
+
+          db.ref('reservations')
+            .orderByChild('travellerId')
+            .equalTo(userDetail.uid)
+            .once('value', history => {
+              if (history.exists()) { userDetail['reservationHistory'] = history.val() }
+
+              response.json({ data: userDetail })
+            })
+        })
+    })
+    .catch(function (error) {
+      console.error(error)
+      response.json({ data: {} })
+    })
+})
+
+module.exports = router
